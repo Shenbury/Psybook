1f9e4784e17d35dd971c3ca8b968810aa0b2e799
Psymon <simonhenbury@live.co.uk>
2025-07-04 22:57:28 +0100

    add migration

--- Psybook.Objects/DbModels/ExperienceRecord.cs
+++ Psybook.Objects/DbModels/ExperienceRecord.cs
@@ -1,10 +1,12 @@
 ﻿using MudBlazor;
 using Psybook.Objects.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace Psybook.Objects.DbModels
 {
     public class ExperienceRecord
     {
+        [Key]
         public required BookingExperience BookingExperience { get; set; }
         public required string Title { get; set; }
         public required string Description { get; set; }
--- Psybook.Shared/Migrations/20250704215715_ImprovedDictionaryModel.Designer.cs
+++ Psybook.Shared/Migrations/20250704215715_ImprovedDictionaryModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Psybook.Shared.Contexts;
 
@@ -11,9 +12,11 @@ using Psybook.Shared.Contexts;
 namespace Psybook.Shared.Migrations
 {
     [DbContext(typeof(BookingContext))]
-    partial class BookingContextModelSnapshot : ModelSnapshot
+    [Migration("20250704215715_ImprovedDictionaryModel")]
+    partial class ImprovedDictionaryModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
