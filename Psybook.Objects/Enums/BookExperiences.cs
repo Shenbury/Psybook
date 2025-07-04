babb98a511b5c097e73cff487c88602037c40a61
Psymon <simonhenbury@live.co.uk>
2025-07-04 10:44:18 +0100

    save

---
+++ Psybook.Objects/Enums/BookExperiences.cs
@@ -0,0 +1,11 @@
+﻿namespace Psybook.Objects.Enums
+{
+    public enum BookExperiences
+    {
+        None = 0,
+        RhinoKeeper = 1,
+        GiraffeKeeper = 2,
+        BigCatsKeeper = 3,
+        VipTour = 4,
+    }
+}
